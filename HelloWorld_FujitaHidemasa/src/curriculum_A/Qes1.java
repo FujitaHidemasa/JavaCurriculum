606264a1016ddb51908a1b058180e355001e59e1
fujitahidemasa <fujitahidemasa@localhost>
2025-09-01 23:01:40 +0900

    課題　変数①　Qes1

---
+++ HelloWorld_FujitaHidemasa/src/curriculum_A/Qes1.java
@@ -0,0 +1,20 @@
+package curriculum_A;
+
+public class Qes1 {
+	
+	/************************************
+	 * 　[概要] 各変数の初期化処理
+	 * 　[詳細] 各型の変数を宣言し、初期値を設定する。
+	 ***********************************/
+	
+	byte byteNum;
+	short shortNum;
+	int intNum;
+	long longNum;
+	float floatNum;
+	double doubleNum;
+	char letter;
+	String letters;
+	boolean isBoolean;
+
+}
