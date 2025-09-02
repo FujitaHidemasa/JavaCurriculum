d9f88a6e32bf1c66698eb89dbfc7c222c4379e82
fujitahidemasa <fujitahidemasa@localhost>
2025-09-02 22:11:42 +0900

    課題　変数②　問３以外提出

---
+++ HelloWorld_FujitaHidemasa/src/curriculum_A/Qes3.java
@@ -0,0 +1,34 @@
+package curriculum_A;
+
+public class Qes3 {
+	/************************************
+	 * 　[概要] 変数を用いて出力
+	 * 　[詳細] 値を代入した変数を用いて出力する
+	 ***********************************/
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int intNum = 11110;
+		System.out.println(intNum);
+		
+		byte byteNum = 20;
+		System.out.println(byteNum);
+		
+		String letters = "a ハロー true";
+		System.out.println(letters);
+		
+		System.out.println(intNum+byteNum);
+		
+		//問３−５
+		
+		
+		
+		double doubleNum = 10.5/100;
+		System.out.println(doubleNum);
+		
+		int Num = 10-100;
+		System.out.println(Num);
+	}
+
+}
