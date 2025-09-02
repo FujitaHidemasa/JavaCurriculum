9cd6057bf065e91c51d8087a99952962cfc00a3a
fujitahidemasa <fujitahidemasa@localhost>
2025-09-02 18:47:37 +0900

    課題　変数　①　修正

--- HelloWorld_FujitaHidemasa/src/curriculum_A/Qes1.java
+++ HelloWorld_FujitaHidemasa/src/curriculum_A/Qes1.java
@@ -7,14 +7,14 @@ public class Qes1 {
 	 * 　[詳細] 各型の変数を宣言し、初期値を設定する。
 	 ***********************************/
 	
-	byte byteNum;
-	short shortNum;
-	int intNum;
-	long longNum;
-	float floatNum;
-	double doubleNum;
-	char letter;
-	String letters;
-	boolean isBoolean;
+	byte byteNum = 0;
+	short shortNum = 0;
+	int intNum = 0;
+	long longNum = 0L;
+	float floatNum = 0.0f;
+	double doubleNum = 0.0;
+	char letter = '\u0000';
+	String letters = null;
+	boolean isBoolean = false;
 
 }
