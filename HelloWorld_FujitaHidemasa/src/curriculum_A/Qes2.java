599ac5a81e0dd920de36b26568446c3a0628aeb4
fujitahidemasa <fujitahidemasa@localhost>
2025-09-02 20:50:28 +0900

    課題　変数　②

---
+++ HelloWorld_FujitaHidemasa/src/curriculum_A/Qes2.java
@@ -0,0 +1,23 @@
+package curriculum_A;
+
+public class Qes2 {
+
+	/************************************
+	 * 　[概要] 各変数に値を代入
+	 * 　[詳細] 宣言した変数に指定の値を代入する
+	 ***********************************/
+	
+	byte byteNum = 10;
+	short shortNum = 100;
+	int intNum = 1000;
+	long longNum = 10000;
+	float floatNum = 9.5f;
+	double doubleNum = 10.5;
+	char letter = 'a';
+	String letters = "ハロー";
+	boolean isBoolean = true;
+	
+	
+	
+	
+}
